ead88934b494c1d01c75b06c77d2369da890158f
DataNerd2021 <chasepattee12@gmail.com>
2025-07-11 08:10:59 -0600

    Refactor API request in playground.py to simplify parameters and add CSV export functionality. Update test.ipynb to reflect changes in data structure and increase execution counts for code cells.

--- playground.py
+++ playground.py
@@ -6,7 +6,7 @@ api_key = 'ZrQEPSkKY2hhc2VwYXR0ZWUxMkBnbWFpbC5jb20'
 num_pages = 10
 
 for page in range(1,num_pages):
-    response = requests.get(f'https://auto.dev/api/listings?apikey={api_key}=&zip=84058&radius=50&body_style[]=crossover&year_min=2021&price_min=20000&price_max=30000&mileage=40000&condition[]=used&driveline[]=AWD&transmission[]=automatic&exclude_no_price=true&page={page}')
+    response = requests.get(f'https://auto.dev/api/listings?apikey={api_key}&page={page}')
     try:
         df = pd.read_csv('listings.csv')
         for listing in response.json()['records']:
@@ -16,4 +16,6 @@ for page in range(1,num_pages):
                 pd.concat([df, pd.json_normalize(listing)])
     except:
         df = pd.json_normalize(response.json()['records'])
+        df.to_csv('listings.csv', index = False)
+        
 print(df)
